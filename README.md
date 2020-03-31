29b107508e4a67cd14b7e3cbb601c36b57d8ac83
Nathan Bernard <nbx-off@outlook.fr>
2020-03-31 19:28:59 +0200

    Added main.c
    + Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jedebutedanslecodeavecflonflon
 C'est mon tout premier projet, je sens que sa va être génial poil aux céréales ! 
+
+Et j'ajoute une ligne
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int merde = 20;
+
+    printf("J'ai %d euros dans ma poche !\n", merde);
+
+    return 0;
+}
