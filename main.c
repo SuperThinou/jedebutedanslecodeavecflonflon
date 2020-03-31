b099663e06cf25a6eebc762725125f0d3b6521d6
Nathan Bernard <nbx-off@outlook.fr>
2020-03-31 19:43:39 +0200

    Updated main.c

--- main.c
+++ main.c
@@ -5,7 +5,13 @@ int main()
 {
     unsigned int merde = 20;
 
-    printf("J'ai %d euros dans ma poche !\n", merde);
+    printf("J'ai %d euros dans ma poche !\n\n", merde);
+
+    printf("Flonflon me vole un peu d'argent\n\n");
+
+    merde = 7;
+
+    printf("Donc il me reste %d euros poil au dos !\n", merde);
 
     return 0;
 }
